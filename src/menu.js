b8cb024981ca1c4e9acc0fe5a8edf4d4479db8ec
Tim Jackson <timjacksonm@gmail.com>
2021-02-15 05:47:14 -0600

    template created for menu items

--- src/menu.js
+++ src/menu.js
@@ -7,7 +7,7 @@ const pizzaFactory = (name, url, description, quantity, price) => {
     return {name, url, description, quantity, price};
   };
 const menuPage = (function() {
-    const pizzaRolls = pizzaFactory('Totino\s Pizza Rolls', '/Resturant-Page/src/images/Totinos.jpg', 'a dish of Italian origin. Open pie made of thin bread dough spread with a spiced mixture of e.g. tomato sauce and cheese', '20ct', '$5.99')
+    const pizzaRolls = pizzaFactory('Totino\s Pizza Rolls', '/Resturant-Page/src/images/Totinos.jpg', 'a dish of Italian origin. Open pie made of thin bread dough spread with a spiced mixture of e.g. tomato sauce and cheese', '20ct', '$5.99');
 
     const menuTab = () => {
         const main = document.querySelector('main')
@@ -16,8 +16,13 @@ const menuPage = (function() {
         main.appendChild(box).className += 'menuBox';
         box = document.querySelector('.menuBox');
 
-        box.appendChild(document.createElement('li')).appendChild(document.createElement('img')).src = pizzaRolls.url;
-        console.log(pizzaRolls);
+        box.appendChild(document.createElement('li'))
+        let box1 = box.querySelector('li');
+        box1.appendChild(document.createElement('p')).textContent = pizzaRolls.name;
+        box1.appendChild(document.createElement('img')).src = pizzaRolls.url;
+        box1.appendChild(document.createElement('p')).textContent = pizzaRolls.price;
+        box1.appendChild(document.createElement('p')).textContent = pizzaRolls.quantity;
+        box1.appendChild(document.createElement('p')).textContent = pizzaRolls.description;
 
     }
 
