21a5caba5b230b8ea173cab8fd19c07a5dc0f79a
Tim Jackson <timjacksonm@gmail.com>
2021-02-15 11:12:10 -0600

    inside my tabFunctions object, replaceChildren() was not being used properly. innerHTML = ''; seems to work better

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const pageFlow = (function() {
     const main = document.querySelector('main')
     const selInputs = document.querySelectorAll('input')
     const tabFunctions = {
-        0: () => {main.replaceChildren(); homePage.homeTab();},
-        1: () => {main.replaceChildren(); menuPage.menuTab();},
-        2: () => {main.replaceChildren(); contactPage.contactTab();},
+        0: () => {main.innerHTML = ''; homePage.homeTab();},
+        1: () => {main.innerHTML = ''; menuPage.menuTab();},
+        2: () => {main.innerHTML = ''; contactPage.contactTab();},
     };
     for (let i = 0; i < selInputs.length; i++) {
         selInputs[i].addEventListener('click', tabFunctions[i]);
